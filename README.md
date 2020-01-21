51073722797b944052dbdcd52f19d5ef59099e69
luozhengjun <luozhengjun@outlook.com>
2020-01-21 06:35:20 +0000

    完善cmake编译流程

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-## Qt和CMake开发Android应用程序
-# 准备开发环境
+# Qt和CMake开发Android应用程序
+## 准备开发环境
 
-# 编译
+## 编译Android APK
   * 设置环境变量
     ```
-    export Qt5_Host=<path to qt gcc_64>
-    export Qt5_Android=<path to qt android>
-    export Qt5_Android_Deploy_Qt=$Qt5_Android/bin/androiddeployqt
+    export Qt_Host=<path to qt gcc_64>
+    export Qt_Android=<path to qt android>
+    export Qt_Android_Deploy_Qt=$Qt5_Android/bin/androiddeployqt
+    export JAVA_HOME=<path to java install directory>
 
     ANDROID_CMAKE_TOOLCHAIN=<path to ANDROID_NDK android.toolchain.cmake>
     ```
@@ -16,7 +17,19 @@
     git clone https://github.com/luozhengjun/qt-android-cmake-tutorial.git
     mkdir build_qt-android-cmake-tutorial
     cd build_qt-android-cmake-tutorial
-    cmake ../android-cmake-tutorial -DCMAKE_TOOLCHAIN_FILE=$ANDROID_CMAKE_TOOCHAIN
+    cmake ../android-cmake-tutorial -DCMAKE_TOOLCHAIN_FILE=$ANDROID_CMAKE_TOOCHAIN -DANDROID_PLATFORM=android-24
+    make apk
+    ```
+  其中`ANDROID_PLATFORM`中api的版本需要`>=24`
+
+## 编译x86_64的HOST版本
+  * 设置环境变量`Qt_Host=<path to qt gcc_64>`
+  * 编译
+    ```
+    git clone https://github.com/luozhengjun/qt-android-cmake-tutorial.git
+    mkdir build_qt-android-cmake-tutorial
+    cd build_qt-android-cmake-tutorial
+    cmake ../android-cmake-tutorial
     make
     ```
 
---
+++ cmake/qt-for-android.cmake
@@ -0,0 +1,64 @@
+cmake_minimum_required(VERSION 3.10)
+
+function(set_qt_search_path)
+    set(CMAKE_FIND_ROOT_PATH "")
+    if(ANDROID)
+        set(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH} "$ENV{Qt_Android}" PARENT_SCOPE)
+    else()
+        set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "$ENV{Qt_Host}" PARENT_SCOPE)
+    endif()
+endfunction()
+
+get_filename_component(deploy_settings_json_in
+    ${CMAKE_CURRENT_LIST_DIR}/deployment-settings.json.in ABSOLUTE
+)
+
+function(add_android_app target)
+    if(NOT ANDROID)
+        return()
+    endif()
+
+    set(list_args SOURCES LIBRARIES)
+    cmake_parse_arguments(_args "" "" "${list_args}" ${ARGN})
+
+    add_library(target SHARED ${_args_SOURCES})
+    target_link_libraries(target ${_args_LIBRARIES})
+
+    set(APP_DIR ${CMAKE_BINARY_DIR}/bin)
+    set(APP_SO_NAME "lib${target}.so")
+    set(APP_SO_DIR ${APP_DIR}/${CMAKE_ANDROID_ARCH_ABI})
+    set(ANDROID_APP_SO ${APP_SO_DIR}/${APP_SO_NAME})
+
+    set_target_properties(target
+        PROPERTIES
+        PREFIX ""
+        SUFFIX ""
+        LIBRARY_OUTPUT_NAME ${APP_SO_NAME}
+        LIBRARY_OUTPUT_DIRECTORY ${APP_SO_DIR}
+    )
+
+    get_filename_component(Qt_Android $ENV{Qt_Android} ABSOLUTE)
+    if(NOT JAVA_HOME)
+    	get_filename_component(JAVA_HOME $ENV{JAVA_HOME} ABSOLUTE)
+    endif()
+    if(NOT ANDROID_SDK)
+        get_filename_component(ANDROID_SDK ${CMAKE_ANDROID_NDK}/.. ABSOLUTE)
+    endif()
+
+    get_filename_component(STDCPP_PATH
+        ${CMAKE_ANDROID_NDK}/sources/cxx-stl/llvm-libc++/libs/${CMAKE_ANDROID_ARCH_ABI}/libc++_shared.so ABSOLUTE)
+
+    set(deploy_settings_json ${CMAKE_BINARY_DIR}/deployment-settings.json)
+    configure_file(${deploy_settings_json_in} ${deploy_settings_json})
+
+    set(android_build_dir ${CMAKE_BINARY_DIR}/android-build)
+    set(android_deploy_qt $ENV{Qt_Android_Deploy_Qt})
+    add_custom_command(
+        OUTPUT build_apk
+        DEPENDS target
+        COMMAND ${CMAKE_COMMAND} -E copy_directory ${APP_DIR} ${android_build_dir}/libs
+	COMMAND ${android_deploy_qt} --output ${android_build_dir} --input ${deploy_settings_json} --android-platform ${ANDROID_PLATFORM} --jdk ${JAVA_HOME} --gradle
+    )
+    add_custom_target(apk DEPENDS build_apk)
+
+endfunction()
